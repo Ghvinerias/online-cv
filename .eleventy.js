f875539c6f75b0264777d4fe50230040395b6da0
Aleksandre Ghvineria <ghvinerias@gmail.com>
2026-02-17 07:09:40 +0000

    Fix Eleventy dev server host/port config

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
 
+  eleventyConfig.setServerOptions({
+    host: "0.0.0.0",
+    port: 8080
+  });
+
   return {
     dir: {
       input: "src",
