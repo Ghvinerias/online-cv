8137d406c1bb56be880b7fa25a39e47116f13c3c
Aleksandre Ghvineria <ghvinerias@gmail.com>
2026-02-17 07:20:13 +0000

    Add markdown filter for templates

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,6 +1,11 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
+
+  const md = markdownIt({ html: true, breaks: true, linkify: true });
+  eleventyConfig.addFilter("markdown", (value = "") => md.render(value));
 
   eleventyConfig.setServerOptions({
     host: "0.0.0.0",
